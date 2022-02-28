9061bd35d05dce4993c5f73e0f05460b653dbbcd
Nico W <nico.wolffld@gmx.de>
2022-02-28 11:15:49 +0100

    Add project files.

---
+++ ASE_Calendar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASE_Calendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
