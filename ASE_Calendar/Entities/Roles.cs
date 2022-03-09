a9d8d0c1a2879ba80ef6f7122022e4daa84d5d55
Nico W <nico.wolffld@gmx.de>
2022-03-09 15:08:03 +0100

    roles enum

---
+++ ASE_Calendar/Entities/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Calendar.Entities
+{
+    public enum Roles
+    {
+        Admin,
+        CarDealer,
+        Employee,
+        Customer
+    }
+}
