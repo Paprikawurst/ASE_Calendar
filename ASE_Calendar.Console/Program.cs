78ff82db8d0fa51d70ec8d0e4813e29de7259d24
AdrianFishy <adrian.fischer94@gmail.com>
2022-03-13 19:37:24 +0100

    Optimized navigation

--- ASE_Calendar.Console/Program.cs
+++ ASE_Calendar.Console/Program.cs
@@ -19,29 +19,30 @@ namespace ASE_Calendar.Console
             var startProgram = true;
             while (startProgram)
             {
-                System.Console.WriteLine("Previous month: 1 | Next month: 2 | Make an Appointment: 3 | Logout: 4 | Close application: 5");
-                var input = System.Console.ReadLine();
+                System.Console.WriteLine("Previous month: Left Arrow | Next month: Right Arrow | Make an Appointment: F1 | Logout: F2 | Close application: F4");
+                var input = System.Console.ReadKey();
                 System.Console.Clear();
-
-                switch (input) 
+               
+                
+                switch (input.Key) 
                 {
-                    case "1":
+                    case ConsoleKey.LeftArrow:
                         System.Console.Clear();
                         Calendar.CreateCalendarPrevMonth();
                         //PreviousMonth.Show();
                         break;
-                    case "2":
+                    case ConsoleKey.RightArrow:
                         System.Console.Clear();
                         Calendar.CreateCalendarNextMonth();
                         //NextMonth.Show();
                         break;
-                    case "3":
+                    case ConsoleKey.F1:
                         //AddAppointment.Create();
                         break;
-                    case "4":
+                    case ConsoleKey.F2:
                         //Logout();
                         break;
-                    case "5":
+                    case ConsoleKey.F4:
                         startProgram = false;
                         break;
                 }
