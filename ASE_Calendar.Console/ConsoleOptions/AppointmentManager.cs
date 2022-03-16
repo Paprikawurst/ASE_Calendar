f52eefb5b98a03e07ada8e86c4ff53a0d8320686
AdrianFishy <adrian.fischer94@gmail.com>
2022-03-16 03:11:08 +0100

    small fix

--- ASE_Calendar.Console/ConsoleOptions/AppointmentManager.cs
+++ ASE_Calendar.Console/ConsoleOptions/AppointmentManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ASE_Calendar.Domain.Entities;
 using ASE_Calendar.Application.Services;
+using System.Text.RegularExpressions;
 
 namespace ASE_Calendar.ConsoleUI.ConsoleOptions
 {
@@ -14,29 +15,73 @@ namespace ASE_Calendar.ConsoleUI.ConsoleOptions
         public DateTime currentTime = DateTime.Now;
         public UserEntity currentUser;
         public DateTime dateSelected;
-        public AppointmentManager(UserEntity currentUser, DateTime currentMonth)
+
+        private enum AppointmentState
+        {
+            userInputDay,
+            userInputTimeSlot,
+            checkInputDay,
+            checkInputTimeSlot,
+        };
+
+        private AppointmentState _appointmentState;
+
+        public AppointmentManager(UserEntity currentUser, DateTime dateSelected)
         {
             this.currentUser = currentUser;
-            this.dateSelected = currentMonth;
+            this.dateSelected = dateSelected;
         }
 
-        public bool CreateAppointment()
+        public void CreateAppointment()
         {
-           
-            
-            Console.WriteLine("Please enter a Day of the current month");
+            string day = "";
+            var timeSlot = "";
 
-            var day = Console.ReadLine();
+            _appointmentState = AppointmentState.userInputDay;
 
-            Console.WriteLine("Please select a timeslot: 8-9 = 1, 9-10 = 2, 10-11 = 3, 11-12 = 4\n13-14 = 5, 14-15 = 6, 15-16 = 7, 16-17 = 8");
-            var timeSlot = Console.ReadLine();
+            switch (_appointmentState)
+            {
+                case AppointmentState.userInputDay:
+
+                    Console.WriteLine("Please enter a Day of the current month");
+                    day = Console.ReadLine();
+                    goto case AppointmentState.checkInputDay;
+
+                case AppointmentState.userInputTimeSlot:
+
+                    Console.WriteLine("Please select a timeslot: 8-9 = 1, 9-10 = 2, 10-11 = 3, 11-12 = 4\n13-14 = 5, 14-15 = 6, 15-16 = 7, 16-17 = 8");
+                    timeSlot = Console.ReadLine();
+                    goto case AppointmentState.checkInputTimeSlot;
+
+                case AppointmentState.checkInputDay:
+
+                    var isNumber = Regex.IsMatch(day, @"^[0-9]*$");
+                    var maxDays = CalendarHelperService.GetMaxMonthDayInt(dateSelected.Month, dateSelected.Year);
+
+                    if (!isNumber || Int16.Parse(day) > maxDays || Int16.Parse(day) <= 0)
+                    {
+                        Console.WriteLine("\n" + "Please enter the correct day!" + "\n");
+                        goto case AppointmentState.userInputDay;
+                    }
+
+                    goto case AppointmentState.userInputTimeSlot;
+
+                case AppointmentState.checkInputTimeSlot:
+
+                    isNumber = Regex.IsMatch(timeSlot, @"^[0-9]*$");
+
+                    if (!isNumber || Int16.Parse(timeSlot) < 1 || Int16.Parse(timeSlot) > 8)
+                    {
+                        Console.WriteLine("\n" + "Please enter a correct time slot!" + "\n");
+                        goto case AppointmentState.userInputTimeSlot;
+                    }
+                    break;
+            }
 
             Date = new DateTime(dateSelected.Year, dateSelected.Month, Int32.Parse(day));
-            
+           
             AppointmentEntity Appointment = new(Date, Int32.Parse(timeSlot), currentUser.userId, Guid.NewGuid());
             AppointmentService.CreateAppointment(Appointment);
-
-            return true;
         }
 
         public void LoadAppointments()
