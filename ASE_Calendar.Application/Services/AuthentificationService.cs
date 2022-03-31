86193f486fb6ebde924f198e60964afd02eaba1d
Nico W <nico.wolffld@gmx.de>
2022-03-31 15:24:29 +0200

    comments on authentification service

--- ASE_Calendar.Application/Services/AuthentificationService.cs
+++ ASE_Calendar.Application/Services/AuthentificationService.cs
@@ -4,30 +4,46 @@ using ASE_Calendar.Domain.Entities;
 
 namespace ASE_Calendar.Application.Services
 {
+    /// <summary>
+    /// A service that manages registration and login.
+    /// </summary>
     public class AuthentificationService
     {
+        /// <summary>
+        /// Starts registration process based on given username, password and role.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="roleId"></param>
         public static void StartRegistration(string username, string password, string roleId)
         {
             UserEntity user = new(username, password, short.Parse(roleId), Guid.NewGuid());
-            UserRepository credentialsToJson = new(user);
+            UserRepository userRepository = new(user);
         }
 
+        /// <summary>
+        /// Start login process based on given username and password.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>
+        /// A user entity or null.
+        /// </returns>
         public static UserEntity StartLogin(string username, string password)
         {
-            UserRepository credentialsRepository = new(username, password);
-            var loggedInUser = credentialsRepository.ReturnUserEntity();
+            UserRepository userRepository = new(username, password);
+            var userEntity = userRepository.ReturnUserEntity();
 
-            if (loggedInUser == null)
+            if (userEntity == null)
             {
                 return null;
             }
 
-            if (loggedInUser.UserDataRegistered.Username == username &&
-                loggedInUser.UserDataRegistered.Password == password)
+            if (userEntity.UserDataRegistered.Username == username &&
+                userEntity.UserDataRegistered.Password == password)
             {
-                return loggedInUser;
+                return userEntity;
             }
-
             return null;
         }
     }
