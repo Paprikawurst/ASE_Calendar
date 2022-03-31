7385ae3edddd5b6cac922db9a549f064fc72793e
Nico W <nico.wolffld@gmx.de>
2022-03-31 15:36:12 +0200

    minor

--- ASE_Calendar.ConsoleUI/ConsoleOptions/AppointmentManager.cs
+++ ASE_Calendar.ConsoleUI/ConsoleOptions/AppointmentManager.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 using ASE_Calendar.Application.Repositories;
 using ASE_Calendar.Application.Services;
 using ASE_Calendar.ConsoleUI.Enums;
 using ASE_Calendar.Domain.Entities;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace ASE_Calendar.ConsoleUI.ConsoleOptions
 {
