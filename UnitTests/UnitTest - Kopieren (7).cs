febe09d38cbd10f7f3551ea0a12d1d036db151b3
Adrian Fischer <adrian.fischer94@gmail.com>
2022-05-23 14:55:59 +0200

    Added unit test template

---
+++ UnitTests/UnitTest - Kopieren (7).cs
@@ -0,0 +1,23 @@
+﻿using ASE_Calendar.Application.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASE_Calendar.Tests
+{
+    [TestClass]
+    public class UnitTest5
+    {
+        [TestMethod]
+        public void CHANGE()
+        {
+            // Arrange
+
+
+
+            // Act
+
+
+            //Assert
+
+        }
+    }
+}
---
+++ UnitTests/UnitTest - Kopieren (8).cs
@@ -0,0 +1,23 @@
+﻿using ASE_Calendar.Application.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASE_Calendar.Tests
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void CHANGE()
+        {
+            // Arrange
+
+
+
+            // Act
+
+
+            //Assert
+
+        }
+    }
+}
